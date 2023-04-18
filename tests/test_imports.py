a2f6b3513926808fcc7307b88dafbdeee9a9c1c3
jarneamerlinck <jarneamerlinck@gmail.com>
2023-04-18 09:38:23 +0200

    added base for tests

---
+++ tests/test_imports.py
@@ -0,0 +1,30 @@
+import pytest
+
+def test_base_import():
+	"""Test base import.
+	"""    
+	import speechtotext
+def test_datasets_import():
+	"""Test datasets import.
+	"""  
+	import speechtotext.datasets
+def test_functions_import():
+	"""Test functions import.
+	"""  
+	import speechtotext.functions
+def test_benchmark_import():
+	"""Test benchmark import.
+	"""  
+	import speechtotext.benchmark
+def test_metric_import():
+	"""Test metric import.
+	"""  
+	import speechtotext.metric
+def test_model_import():
+	"""Test model import.
+	"""  
+	import speechtotext.model
+def test_plot_import():
+	"""Test plot import.
+	"""  
+	import speechtotext.plot
